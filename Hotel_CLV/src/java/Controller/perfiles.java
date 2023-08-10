88bf0c4d382696bacbf1a5a941c2ce4b76fbeb0e
rubendario921 <137409295+rubendario921@users.noreply.github.com>
2023-08-10 11:43:51 -0500

    Implementacion de Clases
    Creacion de Clases para los perfiles

---
+++ Hotel_CLV/src/java/Controller/perfiles.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+/**
+ *
+ * @author Ruben Dario
+ */
+public class perfiles {
+    
+    private Integer perfilId;
+    private String letra;
+    private String nombre;
+
+    public perfiles() {
+    }
+
+    public perfiles(Integer perfilId, String letra, String nombre) {
+        this.perfilId = perfilId;
+        this.letra = letra;
+        this.nombre = nombre;
+    }
+    
+    
+    
+}
