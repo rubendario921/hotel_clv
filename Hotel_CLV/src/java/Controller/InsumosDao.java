834a54cc212ef922858768f3c0215ae409a02b99
Wlady10 <wladycampana@hotmail.com>
2023-08-16 14:52:07 -0500

    ac
    act

--- Hotel_CLV/src/java/Controller/InsumosDao.java
+++ Hotel_CLV/src/java/Controller/InsumosDao.java
@@ -76,4 +76,98 @@ public class InsumosDao {
         return insumo;
     }
 
+    public int crearInsumo(String nombre, String detalle, String cantidad, String valor, String dimg, String esta_id) {
+        int resultado = 0;
+        try {
+            String sql = "INSERT INTO hotel_clv.insumos (insu_nombre, insu_detalle,insu_cantidad,insu_valor,insu_dimg,estados_esta_id) VALUES (?,?,?,?,?,?);";
+            PreparedStatement pst = con.getConexion().prepareCall(sql);
+            pst.setString(1, nombre);
+            pst.setString(2, detalle);
+            pst.setString(3, cantidad);
+            pst.setString(4, valor);
+            pst.setString(5, dimg);
+            pst.setString(6, esta_id);
+            int n = pst.executeUpdate();
+            if (n > 0) {
+                resultado = 1;
+            } else {
+                resultado = 0;
+            }
+            pst.close();
+        } catch (SQLException e) {
+            System.out.println("Error en InsumosDao crearInsumo: " + e.getMessage());
+            int SQLError = e.getErrorCode();
+            switch (SQLError) {
+                case 1062:
+                    resultado = 1062;
+                    break;
+                case 1048:
+                    resultado = 1048;
+                    break;
+                default:
+                    resultado = SQLError;
+                    break;
+            }
+        } finally {
+        }
+        return resultado;
+    }
+
+    public int modificarInsumo(Integer id, String nombre, String detalle, String cantidad, String valor, String dimg, String esta_id) {
+        int resultado = 0;
+        try {
+            String sql = "UPDATE hotel_clv.insumos SET insu_nombre=?, insu_detalle=?,insu_cantidad=?,insu_valor,insu_dimg=?,estados_esta_id=? WHERE insu_id=? ;";
+            PreparedStatement pst = con.getConexion().prepareCall(sql);
+            pst.setString(1, nombre);
+            pst.setString(2, detalle);
+            pst.setString(3, cantidad);
+            pst.setString(4, valor);
+            pst.setString(5, dimg);
+            pst.setString(6, esta_id);
+            pst.setInt(7, id);
+            int n = pst.executeUpdate();
+            if (n > 0) {
+                resultado = 1;
+            } else {
+                resultado = 0;
+            }
+            pst.close();
+        } catch (SQLException e) {
+            System.out.println("Error al eliminar el  formulario: " + e.getMessage());
+            int SQLError = e.getErrorCode();
+            switch (SQLError) {
+                case 1062:
+                    resultado = 1062;
+                    break;
+                case 1048:
+                    resultado = 1048;
+                    break;
+                default:
+                    resultado = SQLError;
+                    break;
+            }
+        } finally {
+        }
+        return resultado;
+    }
+
+    public int eliminarInsumo(Integer id) {
+        int resultado = 0;
+        try {
+            String sql = "DELETE FROM hotel_clv.insumos WHERE insu_id=?;";
+            PreparedStatement pst = con.getConexion().prepareCall(sql);
+            pst.setInt(1, id);
+            int n = pst.executeUpdate();
+            if (n > 0) {
+                resultado = 1;
+            } else {
+                resultado = 0;
+            }
+            pst.close();
+        } catch (SQLException e) {
+            System.out.println("Error en insumosDao eliminarInsumo: " + e.getMessage());
+        } finally {
+        }
+        return resultado;
+    }
 }
---
+++ Hotel_CLV/web/pages_admin/insumos_crear.jsp
@@ -0,0 +1,63 @@
+<%-- 
+    Document   : insumos_crear
+    Created on : 16/08/2023, 13:04:18
+    Author     : Usuario
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<script>
+
+    //Optener valores de los campos
+    function validar formulario() {
+        var nombre = document.getElementById("nombre").value.trim();
+        var detalle = document.getElementById("detalle").value.trim();
+        var cantidad = document.getElementById("cantidad").value.trim();
+        var valor = document.getElementById("valor").value.trim();
+        var insu_dimg = document.getElementById("insu_dimg").value.trim();
+        var estados_esta_id = document.getElementById("estados_esta_id").value.trim();
+
+        //Validar campos obligatorios
+
+        if (letra === " " || nombre === "") {
+            alert("Por favor, completar todos los campos.");
+            return false;
+        }
+        return true;
+    }
+
+</script>
+<div id="page-wrapper">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Registro Insumo</h1>
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3>Nuevo Insumo</h3>
+                    </div>
+                    <div class="panel-body">
+                        <form action="Mantenimiento_admin/crudunsumos_registrar.jsp" method="POST"onsubmit="return validarFormulario()">
+                            <label> Nombre: </label><input type="text" class="form-control" name="nombre"  placeholder="Ingrese el nombre del insumo. " maxlength="15" required/>
+                            <label> Detalle: </label><input type="text" class="form-control" name="detalle"  placeholder="Ingrese el detalle insumo. " maxlength="100" required/>
+                            <label> Cantidad: </label><input type="text" class="form-control" name="cantidad"  placeholder="Ingrese la cantidad del insumo. " maxlength="20" required/>
+                            <label> Valor: </label><input type="text" class="form-control" name="valor"  placeholder="Ingrese el valor del insumo. " maxlength="3,2" required/>
+                            <label> Imagen: </label><input type="text" class="form-control" name="insu_dimg"  placeholder="Ingrese la imagen del insumo. " maxlength="255" required/>
+                            <label> Estado: </label><input type="text" class="form-control" name="estados_esta_id"  placeholder="Ingrese el estado del insumo. " maxlength="" required/>
+                            <br>
+                            <a href="insumos_lista.jsp" class="btn btn-danger">Regresar</a>
+                            <input type="submit" value="Registrar" name="nuevo_insumo" class="btn bg-primary"/>
+                        </form>
+                         </div>
+                </div>
+            </div>            
+        </div>        
+    </div>    
+</div>
+<!--Fin del Cuerpo -->
+<%@include file="template/footer_admin.jsp" %>
+                        
+                        
+                        
+                        
+                    </div>
